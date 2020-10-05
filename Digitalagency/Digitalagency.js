bb52964144eb65a6f55df027507039702a38bd2c
Hien-BT01 <hienbui25g@gmail.com>
2020-10-05 23:36:16 +0700

    new update

--- Digitalagency/Digitalagency.js
+++ Digitalagency/Digitalagency.js
@@ -23,3 +23,67 @@ $(window).scroll(function(){
    }
 })
 
+$(window).scroll(function(){
+    if($(this).scrollTop()>50){
+        $('.header-navbar-side-button').fadeIn();
+        $('.header-navbar').addClass('header_navbar_fix');
+        $('.header-nav-logo img').attr('src','http://demo.themelogi.com/navian/wp-content/themes/navian/assets/img/logo-dark.png');
+    }else{
+        $('.header-navbar').removeClass('header_navbar_fix');
+        $('.header-nav-logo img').attr('src','http://demo.themelogi.com/navian/wp-content/themes/navian/assets/img/logo-light.png');
+    }
+})
+$('.header-navbar-side-button').click(function(){
+    $("html").animate({ scrollTop: 0 }, 500);
+})
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayHoverPause:true,
+    autoplayTimeout:2000,
+    margin:10,
+    nav:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4,
+        }
+    }
+});
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    nav:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },            
+        960:{
+            items:5
+        },
+        1200:{
+            items:6
+        }
+    }
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
+
+// using the event helper

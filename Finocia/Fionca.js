cd134f7d4088ca4e9843956437409aebd9d4a5fc
Hien-BT01 <hienbui25g@gmail.com>
2020-10-25 13:47:48 +0700

    Fionca completed

--- Finocia/Fionca.js
+++ Finocia/Fionca.js
@@ -1,13 +1,43 @@
-let flipAnimation = document.querySelectorAll(".animation_flip");
-
+// this is for after clicking once, we have to moveOut the mouse over the element, then we can click again
+var flipAnimation = document.querySelectorAll(".animation_flip");
 flipAnimation.forEach((flipAnimation) => {
     flipAnimation.addEventListener("click", (event) => {
-        flipAnimation.classList.toggle("animate__flipInY");
-        if (flipAnimation.classList.contains("animate__flipInY")) {
-            flipAnimation.classList.remove("animate__flipInY");
-        }
-        setTimeout(function () {
-            flipAnimation.classList.add("animate__flipInY");
-        }, 10);
+        flipAnimation.classList.toggle("flipped");
     });
+    flipAnimation.addEventListener("mouseleave", () => {
+        flipAnimation.classList.remove("flipped");
+    })
 });
+
+// scroll Button to bottom and turn back
+var scrolltoTop = document.querySelector('.button__scroll-top');
+window.addEventListener('scroll', function () {
+    if (document.documentElement.scrollTop > 200) {
+        scrolltoTop.classList.add('scroll_toTop');
+    } else {
+        scrolltoTop.classList.remove('scroll_toTop');
+    }
+})
+scrolltoTop.addEventListener('click', () => {
+    $('html').animate({
+        scrollTop: 0
+    }, 1000);
+})
+// $(window).scroll(function () {
+//     if ($(window).scrollTop() >= 200) {
+//         $('.button__scroll-top').addClass('scroll_toTop');
+//     } else {
+//         $('.button__scroll-top').removeClass('scroll_toTop');
+//     }
+// })
+// $('.button__scroll-top').click(function () {
+//     $('html').animate({
+//         scrollTop: 0
+//     }, 1000);
+// });
+const demos = document.querySelector('#Demos');
+const feature = document.querySelector("#Feature");
+
+demos.addEventListener('click', () => {
+    demos.style.transition = '0.5s';
+})

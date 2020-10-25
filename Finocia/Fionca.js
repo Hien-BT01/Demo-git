89c4df8527ed53a5dbbba59f11ad8de6680838ef
Hien-BT01 <hienbui25g@gmail.com>
2020-10-25 14:07:47 +0700

    new update

--- Finocia/Fionca.js
+++ Finocia/Fionca.js
@@ -10,24 +10,12 @@ flipAnimation.forEach((flipAnimation) => {
 });
 
 // scroll Button to bottom and turn back
-var scrolltoTop = document.querySelector('.button__scroll-top');
-window.addEventListener('scroll', function () {
-    if (document.documentElement.scrollTop > 200) {
-        scrolltoTop.classList.add('scroll_toTop');
-    } else {
-        scrolltoTop.classList.remove('scroll_toTop');
-    }
-})
-scrolltoTop.addEventListener('click', () => {
-    $('html').animate({
-        scrollTop: 0
-    }, 1000);
-})
-// $(window).scroll(function () {
-//     if ($(window).scrollTop() >= 200) {
-//         $('.button__scroll-top').addClass('scroll_toTop');
+// var scrolltoTop = document.querySelector('.button__scroll-top');
+// window.addEventListener('scroll', function () {
+//     if (document.documentElement.scrollTop > 200) {
+//         scrolltoTop.classList.add('scroll_toTop');
 //     } else {
-//         $('.button__scroll-top').removeClass('scroll_toTop');
+//         scrolltoTop.classList.remove('scroll_toTop');
 //     }
 // })
 // $('.button__scroll-top').click(function () {
@@ -35,9 +23,20 @@ scrolltoTop.addEventListener('click', () => {
 //         scrollTop: 0
 //     }, 1000);
 // });
-const demos = document.querySelector('#Demos');
-const feature = document.querySelector("#Feature");
-
-demos.addEventListener('click', () => {
-    demos.style.transition = '0.5s';
+// scrolltoTop.addEventListener('click', () => {
+//     $('html').animate({
+//         scrollTop: 0
+//     }, 1000);
+// })
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 200) {
+        $('.button__scroll-top').addClass('scroll_toTop');
+    } else {
+        $('.button__scroll-top').removeClass('scroll_toTop');
+    }
 })
+$('.button__scroll-top').click(function () {
+    $('html').animate({
+        scrollTop: 0
+    }, 50);
+});

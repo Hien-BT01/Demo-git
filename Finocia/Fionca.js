de17447d0b24bb6f4d72cb65970673d6875847b9
Hien-BT01 <hienbui25g@gmail.com>
2020-10-27 09:46:08 +0700

    new update

--- Finocia/Fionca.js
+++ Finocia/Fionca.js
@@ -1,14 +1,3 @@
-// this is for after clicking once, we have to moveOut the mouse over the element, then we can click again
-var flipAnimation = document.querySelectorAll(".animation_flip");
-flipAnimation.forEach((flipAnimation) => {
-    flipAnimation.addEventListener("click", (event) => {
-        flipAnimation.classList.toggle("flipped");
-    });
-    flipAnimation.addEventListener("mouseleave", () => {
-        flipAnimation.classList.remove("flipped");
-    })
-});
-
 // scroll Button to bottom and turn back
 // var scrolltoTop = document.querySelector('.button__scroll-top');
 // window.addEventListener('scroll', function () {
@@ -28,10 +17,25 @@ flipAnimation.forEach((flipAnimation) => {
 //         scrollTop: 0
 //     }, 1000);
 // })
+// this is for after clicking once, we have to moveOut the mouse over the element, then we can click again
+
+var flipAnimation = document.querySelectorAll(".animation_flip");
+flipAnimation.forEach((flipAnimation) => {
+    flipAnimation.addEventListener("click", (event) => {
+        flipAnimation.classList.toggle("flipped");
+    });
+    flipAnimation.addEventListener("mouseleave", () => {
+        flipAnimation.classList.remove("flipped");
+    })
+});
+
+
+// scroll down button top
 $(window).scroll(function () {
     if ($(window).scrollTop() >= 200) {
         $('.button__scroll-top').addClass('scroll_toTop');
     } else {
+        $('.button__scroll-top').css('transition', '1.5s');
         $('.button__scroll-top').removeClass('scroll_toTop');
     }
 })
@@ -40,3 +44,14 @@ $('.button__scroll-top').click(function () {
         scrollTop: 0
     }, 50);
 });
+
+const flowUpItems1 = document.querySelectorAll('.flow-up-item-1');
+const flowUpItems2 = document.querySelectorAll('.flow-up-item-2')
+
+// flow up items
+
+// $(window).scroll(function () {
+//     if ($(window).scrollTop() >= 2800) {
+
+//     }
+// })

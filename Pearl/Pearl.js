0a9c4b1a426f3ab5a6cb2643973cdfd8feb04bc3
Hien-BT01 <hienbui25g@gmail.com>
2020-12-14 23:16:44 +0700

    new update

--- Pearl/Pearl.js
+++ Pearl/Pearl.js
@@ -1,49 +1,70 @@
-let buttonDesmosCustomMades = document.querySelectorAll("[data-button-target]");
+let buttonDesmosCustomMades = document.querySelectorAll("[data-filter]");
 let imageDemosCustomMades = document.querySelectorAll("[data-content]");
+let $itemList = $(".Demos-list__items");
+$itemList.isotope({
+    itemSelector: '.All',
+    layoutMode: 'masonry'
+});
 buttonDesmosCustomMades.forEach(tab => {
     tab.addEventListener('click',() => {
-        const buttonTarget = tab.getAttribute('data-button-target');
-        buttonDesmosCustomMades.forEach(tab => {
-            tab.classList.remove("colorChecked");
+        let buttonTarget = tab.getAttribute('data-filter');
+        buttonDesmosCustomMades.forEach(buttonClick => {
+            buttonClick.classList.remove("colorChecked");
         })
         tab.classList.add("colorChecked");
-        imageDemosCustomMades.forEach((imageDemosCustomMade) => {
-            const dataContents = imageDemosCustomMade.getAttribute('data-content').split(" ");
-            //imageDemosCustomMade.classList.add("zoom-in");
-            // let timeOut = setTimeout(function(){
-            //     imageDemosCustomMade.classList.add("noneChecked");
-            // },10)
-            // imageDemosCustomMade.classList.add("noneChecked");
-        
-            imageDemosCustomMade.classList.remove("zoom-out");
-            imageDemosCustomMade.classList.add("zoom-in");
-            imageDemosCustomMade.classList.add("noneChecked");
-            // console.log($(imageDemosCustomMade));
-            dataContents.forEach((e) => {
-                if(e === buttonTarget) {
-                    // imageDemosCustomMade.classList.remove("zoom-in");
-                    // $(imageDemosCustomMade).show();
-                    imageDemosCustomMade.classList.remove("noneChecked");
-                    imageDemosCustomMade.classList.add("zoom-out");
-                    imageDemosCustomMade.classList.remove("zoom-in");
-                    // clearTimeout(timeOut);
-                    // setTimeout(function(){
-                    //     imageDemosCustomMade.classList.add("zoom-out");
-                    // },10);
-                        // setTimeout(function(){
-                        //     $(imageDemosCustomMade).hide()
-                        // },300);
-                        // console.log($(imageDemosCustomMade));
-                }
-            })
+        $itemList.isotope({
+            filter: buttonTarget
         })
     })
 })
+let dropDownMenu = document.querySelector(".Demos-list__topics");
+let dropdownIcon = document.querySelector(".dropdown-button__list__icon");
+dropdownIcon.addEventListener("click",(e) => {
+    dropdownIcon.classList.toggle("rotate");
+    dropDownMenu.classList.toggle("active");
+})
+
+$(window).scroll(function(){
+    if($(window).scrollTop() >= 50){
+        $(".header-nav").css("transition","0.3s");
+        $(".header-nav").addClass("fix-navbar");
+    }else{
+        $(".header-nav").removeClass("fix-navbar")
+    }
+})
+$(".dropdown-menu__button").click(function(e){
+    e.stopPropagation()
+    $(".navbar-item").toggleClass("dropdown__res");
+    if($(window).width() < 975){
+        $(".header-nav .container").toggleClass("nav__active");
+    }
+});
+$(document).ready(function(){
+    $(window).resize(function(){
+        if($(this).width() < 975){
+            $(".logo img").attr("src","https://pearl.stylemixthemes.com/landing/assets/images/header/logo-white.png");
+        }else{
+            $(".logo img").attr("src","https://pearl.stylemixthemes.com/landing/assets/images/header/logo.png");
+        }
+    })
+});
+$(".menu-list__items-icon").click(function(e){
+    e.stopPropagation();
+    $(this).toggleClass("updown");
+    $(this).next().toggleClass("activeSub");
+});
+// let subMenus = document.querySelectorAll(".menu-list__items-icon");
+// subMenus.forEach(subMenu => {
+//     subMenu.addEventListener("click",(e) => {
+//         e.stopPropagation();
+//         subMenu.classList.toggle("updown");
+//         subMenu.nextElementSibling.classList.toggle("activeSub");
+//     })
+// })
+
 
 // const btnTab = document.querySelectorAll("[data-button-target]");
 // const items = document.querySelectorAll("[data-content]");
-
-
 // btnTab.forEach((btn) => {
 //     btn.onclick = function() {
 //         const btnTarget = this.getAttribute('data-button-target');

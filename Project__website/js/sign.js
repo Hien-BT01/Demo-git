3389f8146a1435b0ec1ae7c2dbb58302d0b400d9
Hien-BT01 <hienbui25g@gmail.com>
2021-01-05 17:15:38 +0700

    sigin/signup form

--- Project__website/js/sign.js
+++ Project__website/js/sign.js
@@ -1,15 +1,13 @@
-let submit = document.querySelector(".submit");
-
-
-submit.addEventListener("click",(e) => {
+function ribbleClick(element,e){
+    console.log(element)
     let btn_top_pos = e.offsetY;
     let btn_left_pos = e.offsetX;
     const spanElement = document.createElement("span");
     spanElement.classList.add("effect-click");
     spanElement.style.top = `${btn_top_pos}px`;
     spanElement.style.left = `${btn_left_pos}px`;
-    submit.appendChild(spanElement);
+    element.appendChild(spanElement);
     setTimeout(() => {
-        submit.removeChild(spanElement);
+        element.removeChild(spanElement);
     },300)
-}) 
+}
